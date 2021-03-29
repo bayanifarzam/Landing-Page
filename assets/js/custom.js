dbdc868e463bb234d05a6aaa364ad3dfdf713783
Raza Farzam <bayanifarzam@gmail.com>
2021-03-29 15:46:14 +0500

    jquery added

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+
+$(function(){
+
+
+
+// $('.msg').on('click',function(){
+//         $('#alertSuccess').removeClass('d-none');
+// });
+
+
+$('.msg').click(function(){
+    $('.msg').hide();
+
+});
+
+
+
+
+
+
+
+});
